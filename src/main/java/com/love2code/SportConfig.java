e80f457c7bbfa7b9f8a2c5b4d01ad3490d1874cd
msnrifat <misranrifatnijat@gmail.com>
2020-10-17 20:25:27 -0400

    @PropertySource

--- src/main/java/com/love2code/SportConfig.java
+++ src/main/java/com/love2code/SportConfig.java
@@ -2,9 +2,11 @@ package com.love2code;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 // @ComponentScan("com.luv2code.springdemo")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
 	
 	// define bean for our sad fortune service
--- src/main/java/com/love2code/SwimCoach.java
+++ src/main/java/com/love2code/SwimCoach.java
@@ -1,23 +1,38 @@
 package com.love2code;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 
-	private FortuneService fortuneService;
+    private FortuneService fortuneService;
 
-	public SwimCoach(FortuneService theFortuneService) {
-		fortuneService = theFortuneService;
-	}
-	
-	@Override
-	public String getDailyWorkout() {
-		return "Swim 1000 meters as a warm up.";
+    @Value("${foo.email}")
+    private String email;
+
+    @Value("${foo.team}")
+    private String team;
+
+    public SwimCoach(FortuneService theFortuneService) {
+        fortuneService = theFortuneService;
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Swim 1000 meters as a warm up.";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
+
+	public String getEmail() {
+		return email;
 	}
 
-	@Override
-	public String getDailyFortune() {
-		return fortuneService.getFortune();
+	public String getTeam() {
+		return team;
 	}
-
 }
 
 
